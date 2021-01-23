acc33a97c4f5ee3d1d93c7c8c70c0fdeae854ad3
FlutLab.io <>
2021-01-23 08:09:41 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            MaterialButton(
+               onPressed: _incrementCounter,
+               shape: StadiumBorder(),
+               height: 50.0,
+               minWidth: 150.0,
+               textColor: Colors.white,
+               splashColor:Colors.white,
+               color:Colors.red,
+               child: Text(
+                 "Click Me",
+                 style: TextStyle(
+                   fontSize:15.0,
+                 ),
+               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
